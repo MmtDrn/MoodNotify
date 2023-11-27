35c72545751b4ad72848d581342c40d28a213646
Mehmet Duran <mehmet.durann@gmail.com>
2023-11-28 01:51:03 +0300

    - loginView built

--- MoodNotify/MoodNotify/LoginView/LoginView.swift
+++ MoodNotify/MoodNotify/LoginView/LoginView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("LoginView")
+        NavigationStack {
+            VStack(spacing: .heightSize(20)) {
+                Text("MoodNotify")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.colorButton)
+                
+                CircleView()
+                
+                Text("MoodNotify allows you to instantly share your feelings or what you're up to, stay connected with friends, and explore different moods around you!")
+                    .padding(.horizontal)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(LinearGradient(colors: [.colorWomen.opacity(2), .colorMan.opacity(2)], startPoint: .leading, endPoint: .trailing))
+                
+                LoginButtonsView()
+            }
+        }
     }
 }
 
