b18c590613c1d383d9a21e31c3499353bd43592e
Mehmet Duran <mehmet.durann@gmail.com>
2023-11-27 12:19:08 +0300

    - first init

---
+++ MoodNotify/MoodNotify/App/MoodNotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodNotifyApp.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodNotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ MoodNotify/MoodNotify/LoginView/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 27.11.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("LoginView")
+    }
+}
+
+#Preview {
+    LoginView()
+}
