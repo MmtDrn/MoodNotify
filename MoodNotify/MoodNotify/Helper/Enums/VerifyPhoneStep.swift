bdd9d35daa4dd10fd2c8bb83817743fdf6363c14
Mehmet Duran <mehmet.durann@gmail.com>
2023-11-29 15:13:42 +0300

    - verifyPhone step enum added

---
+++ MoodNotify/MoodNotify/Helper/Enums/VerifyPhoneStep.swift
@@ -0,0 +1,13 @@
+//
+//  VerifyPhoneStep.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 29.11.2023.
+//
+
+import Foundation
+
+enum VerifyPhoneStep: Int {
+    case phoneNumber
+    case code
+}
