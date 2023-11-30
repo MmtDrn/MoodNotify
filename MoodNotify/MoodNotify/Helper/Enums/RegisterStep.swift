b99100f48d0ce6c3bb039e5e8de61bff5a034ce5
Mehmet Duran <mmt.durann@gmail.com>
2023-11-30 13:22:30 +0300

    - registerStep enum added, stepBarView built

---
+++ MoodNotify/MoodNotify/Helper/Enums/RegisterStep.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterStep.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 30.11.2023.
+//
+
+import Foundation
+
+enum RegisterStep: Int, CaseIterable {
+    case nameBirthday
+    case gender
+    case interest
+    case photos
+}
---
+++ MoodNotify/MoodNotify/Views/RegisterView/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 30.11.2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RegisterView()
+}
