9c5f9ae0bc9b0b300167276638eb042d58f2d0e4
Mehmet Duran <mmt.durann@gmail.com>
2023-12-21 12:54:44 +0300

    - shake effect added to view, userAppearanceModel added

---
+++ MoodNotify/MoodNotify/Model/UserAppearanceModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserAppearanceModel.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 20.12.2023.
+//
+
+import Foundation
+
+struct UserAppearanceModel {
+    var fullName: String?
+    var birthday: String?
+    var genderType: Int?
+    var interests: [Int]?
+    var imagesURL: [String]?
+}
