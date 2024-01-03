9d936194c76692f0b0561d1109f2d68f4e4c06a7
Mehmet Duran <mmt.durann@gmail.com>
2024-01-03 11:45:50 +0300

    - firebase packages and setup requirements added

---
+++ MoodNotify/MoodNotify/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MoodNotify
+//
+//  Created by mehmet duran on 3.01.2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
