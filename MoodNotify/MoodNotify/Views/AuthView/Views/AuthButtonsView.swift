df5200a8e0e0cb13f7fce002363a76023b05fac7
Mehmet Duran <mmt.durann@gmail.com>
2024-01-04 23:46:41 +0300

    - SignInWithAppleButton added to authButtons and layout changed

--- MoodNotify/MoodNotify/Views/AuthView/Views/AuthButtonsView.swift
+++ MoodNotify/MoodNotify/Views/AuthView/Views/AuthButtonsView.swift
@@ -6,37 +6,27 @@
 //
 
 import SwiftUI
+import _AuthenticationServices_SwiftUI
 
 struct AuthButtonsView: View {
+    @Environment(\.colorScheme) var theme
     
-    var loginWithAppleAction: () -> Void
     var loginWithGooleAction: () -> Void
-    
+    var handleAppleRequest: (ASAuthorizationAppleIDRequest) -> Void
+    var handleAppleCompletion: (Result<ASAuthorization, Error>) -> Void
     
     var body: some View {
         VStack(spacing: .heightSize(15)) {
             
-            Button(action: {
-                loginWithAppleAction()
-            }) {
-                Image(systemName: "apple.logo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: .widthSize(20), height: .heightSize(20))
-                    .foregroundStyle(.white)
-                
-                Text("With Apple")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.white)
+            SignInWithAppleButton(.continue) { request in
+                handleAppleRequest(request)
+            } onCompletion: { result in
+                handleAppleCompletion(result)
             }
-            .frame(width: .widthSize(260), height: .heightSize(40))
-            .padding(.horizontal)
-            .padding(.vertical, 5)
-            .background(
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.black)
-            )
+            .frame(maxWidth: .infinity)
+            .frame(height: .heightSize(60))
+            .padding(.horizontal, .widthSize(50))
+            .signInWithAppleButtonStyle(theme == .dark ? .white : .black)
             
             Button(action: {
                 loginWithGooleAction()
@@ -46,24 +36,30 @@ struct AuthButtonsView: View {
                     .scaledToFill()
                     .frame(width: .widthSize(40), height: .heightSize(40))
                 
-                Text("With Google")
+                Text("Contuniue with Google")
                     .font(.title2)
                     .fontWeight(.semibold)
                     .foregroundStyle(.gray)
             }
-            .frame(width: .widthSize(260), height: .heightSize(40))
-            .padding(.horizontal)
-            .padding(.vertical, 5)
+            .frame(maxWidth: .infinity)
+            .frame(height: .heightSize(58))
             .background(
                 RoundedRectangle(cornerRadius: 10)
                     .foregroundStyle(.white)
                     .shadow(radius: 2)
+                    .padding(.horizontal, .widthSize(50))
             )
+            
         } //: VStack
         .padding(.top, .heightSize(40))
     }
 }
 
 #Preview {
-    AuthButtonsView(loginWithAppleAction: {}, loginWithGooleAction: {})
+    AuthButtonsView(loginWithGooleAction: {}) { request in
+        
+    } handleAppleCompletion: { result in
+        
+    }
+
 }
